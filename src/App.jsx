4d32603ae35c6c4ca18ae1489fb8f924baad54b2
Chandan <ethicalrobo06@gmail.com>
2024-07-12 23:53:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Highlights from "./components/Highlights"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Model from "./components/Model"
+
+import * as Sentry from '@sentry/react'
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App)
